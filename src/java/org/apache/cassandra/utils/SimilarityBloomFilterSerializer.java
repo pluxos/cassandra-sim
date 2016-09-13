2314740fcc23e872cb91f985194f51271840e3c0
mourao666 <mourao@lntb-mul037628.network.ctbc>
2016-09-13 15:31:43 -0300

    Change metgod deserialize in SimilarityBloomFilterSerializer to use LocalitySensitiveBitSet

--- src/java/org/apache/cassandra/utils/SimilarityBloomFilterSerializer.java
+++ src/java/org/apache/cassandra/utils/SimilarityBloomFilterSerializer.java
@@ -24,6 +24,7 @@ import org.apache.cassandra.db.TypeSizes;
 import org.apache.cassandra.io.ISerializer;
 import org.apache.cassandra.io.util.DataOutputPlus;
 import org.apache.cassandra.utils.obs.IBitSet;
+import org.apache.cassandra.utils.obs.LocalitySensitiveBitSet;
 import org.apache.cassandra.utils.obs.OffHeapBitSet;
 import org.apache.cassandra.utils.obs.OpenBitSet;
 
@@ -44,7 +45,8 @@ public class SimilarityBloomFilterSerializer implements ISerializer<SimilarityBl
     public SimilarityBloomFilter deserialize(DataInput in, boolean offheap) throws IOException
     {
         int hashes = in.readInt();
-        IBitSet bs = offheap ? OffHeapBitSet.deserialize(in) : OpenBitSet.deserialize(in);
+        /*IBitSet bs = offheap ? OffHeapBitSet.deserialize(in) : OpenBitSet.deserialize(in);*/
+        IBitSet bs = LocalitySensitiveBitSet.deserialize(in);
         return createFilter(hashes, bs);
     }
 
